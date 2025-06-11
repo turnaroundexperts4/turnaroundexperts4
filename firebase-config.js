c7b89fd09da7b4596ebdcf4dfee46e4f8370975e
turnaroundexperts4 <tempmail310107@gmail.com>
2025-06-12 05:23:07 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDdUEW0_Lmv8jj48jQBCS0CM5wbRJeNVn8",
-  authDomain: "turnaroundexperts-113b9.firebaseapp.com",
-  projectId: "turnaroundexperts-113b9",
-  storageBucket: "turnaroundexperts-113b9.firebasestorage.app",
-  messagingSenderId: "210110687712",
-  appId: "1:210110687712:web:f81df5bb06b70712ec6385"
-};
-export const app = firebase.initializeApp(firebaseConfig);
